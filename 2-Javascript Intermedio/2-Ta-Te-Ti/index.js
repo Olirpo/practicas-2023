871e9704aa2d2264ec0fa734f38c2e2e7c8f08a2
Joaquin <joaquinfontana11@gmail.com>
2023-06-06 17:08:22 -0300

    Comenzada funcionalidades basicas del ta-te-ti, funcion de buscar ganador, etc

--- 2-Javascript Intermedio/2-Ta-Te-Ti/index.js
+++ 2-Javascript Intermedio/2-Ta-Te-Ti/index.js
@@ -11,31 +11,38 @@ const jugar = async () => {
   for (let i = 0; i < 3; i++) {
     matrix.push([]);
     for (let j = 0; j < 3; j++) {
-      matrix[i].push([]);
+      matrix[i].push("");
     }
   }
   console.log("Bienvenido al ultimo TA-TE-TI!");
   console.log("Para jugar necesitarás un amigo");
 
   while (turno < 9) {
+    let coords = {};
+    console.table(matrix);
     const jugador = turno % 2 === 0 ? "Jugador 1" : "Jugador 2";
     const figuraJugador = turno % 2 === 0 ? J1 : J2;
-    console.log("Turno del ", jugador);
-    const { y, x } = await turnoJugador();
-    if (!isPositionValid({ y, x }, matrix)) {
-      console.log("Posicion en uso");
+    console.log("Turno del ", jugador, ": ", figuraJugador);
+    coords = await turnoJugador();
+
+    while (!isPositionValid(coords, matrix)) {
+      console.clear();
+      console.table(matrix);
+      console.log("Esa posicion ya está en uso");
+      console.log("Seleccione otra");
+      coords = await turnoJugador();
     }
-    matrix[y][x] = figuraJugador;
+    matrix[coords.y][coords.x] = figuraJugador;
     turno++;
   }
 };
 
 const turnoJugador = async (jugador) => {
-  let y =
+  let x =
     (await readline.question(
       "Seleccione en que columna desea jugar su turno: "
     )) - 1;
-  let x =
+  let y =
     (await readline.question("Seleccione en que fila desea jugar su turno: ")) -
     1;
   readline.pause();
@@ -43,6 +50,28 @@ const turnoJugador = async (jugador) => {
 };
 const isPositionValid = (coords, matrix) => {
   const { y, x } = coords;
-  return Boolean(matrix[y][x]);
+  return !Boolean(matrix[y][x]);
 };
-jugar();
+const checkForWinner = (matrix) => {
+  //Chequeo Filas
+  for (let i = 0; i < matrix.lenght; i++) {
+    if (matrix[i].every((e) => e === "o")) {
+      return [true, "GANADORRR o, jugador 1! Felicitaciones!"];
+    } else if (matrix[i].every((e) => e === "x")) {
+      return [true, "GANADORRR x, jugador 2! Felicitaciones!"];
+    } // Chequeo columnas
+    else if (matrix.every((e) => e[index] === "o")) {
+      return [true, "GANADORRR o, jugador 1! Felicitaciones!"];
+    } else if (matrix.every((e) => e[index] === "x")) {
+      return [true, "GANADORRR x, jugador 2! Felicitaciones!"];
+    }
+  }
+};
+//jugar();
+console.log(
+  checkForWinner([
+    ["o", "x", "o"],
+    ["o", "o", "o"],
+    ["o", "x", "x"],
+  ])
+);
